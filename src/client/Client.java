565731b3a12a6d6dfd7dc794e96120ee81dacb0f
Danielyuuuuu <48498200+Danielyuuuuu@users.noreply.github.com>
2021-05-08 18:48:23 +1000

    Server RMI doesn't work

---
+++ src/client/Client.java
@@ -0,0 +1,24 @@
+package client;
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+import remote.IRemoteWhiteBoard;
+
+public class Client {
+	public static void main(String[] args) {
+		try {
+			//Connect to the rmiregistry that is running on localhost
+			Registry registry = LocateRegistry.getRegistry("localhost");
+           
+			//Retrieve the stub/proxy for the remote math object from the registry
+			IRemoteWhiteBoard remoteWhiteBoard = (IRemoteWhiteBoard) registry.lookup("whiteBoard");
+			
+			System.out.println("Client: calling remote methods");
+			remoteWhiteBoard.printHello();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
